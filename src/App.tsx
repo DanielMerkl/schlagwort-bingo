60c9a3f0877d298f5b4874e1974860ec6b614e2d
Daniel Merkl <daniel.merkl.dm@gmail.com>
2021-01-05 16:49:35 +0100

    implemented app layout (header with pages)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,33 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import styled from "styled-components";
 
-export const App = () => <h1>cleanup</h1>;
+import { Path } from "./typing/enum/path";
+import { Header } from "./components/Header";
+import { HomePage } from "./pages/home/HomePage";
+import { CreateGamePage } from "./pages/createGame/CreateGamePage";
+import { JoinGamePage } from "./pages/joinGame/JoinGamePage";
+
+export const App = () => (
+  <AppWrapper>
+    <Header />
+    <Switch>
+      <Route exact path={Path.Home}>
+        <HomePage />
+      </Route>
+      <Route path={Path.CreateGame}>
+        <CreateGamePage />
+      </Route>
+      <Route path={Path.JoinGame}>
+        <JoinGamePage />
+      </Route>
+      <Redirect to={Path.Home} />
+    </Switch>
+  </AppWrapper>
+);
+
+const AppWrapper = styled.div`
+  height: 100%;
+  display: grid;
+  grid-template-rows: auto 1fr;
+`;
