1216e1955a2c24fb06aa4df5f54c4bdd31c2122a
Daniel Merkl <daniel.merkl.dm@gmail.com>
2021-01-05 20:19:08 +0100

    fixed bug with styles

--- src/components/ComposedProviders.tsx
+++ src/components/ComposedProviders.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
-import { ThemeProvider, StylesProvider } from "@material-ui/styles";
+import { ThemeProvider, StylesProvider } from "@material-ui/core/styles";
 
 import { theme } from "../utils/theme";
 import { UserContextProvider } from "../context/UserContext";
@@ -9,15 +9,15 @@ import { SnackbarContextProvider } from "../context/SnackbarContext";
 
 export const ComposedProviders: FC = ({ children }) => (
   <BrowserRouter>
-    <UserContextProvider>
-      <SnackbarContextProvider>
-        <StylesProvider injectFirst>
-          <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme}>
+      <StylesProvider injectFirst>
+        <UserContextProvider>
+          <SnackbarContextProvider>
             <CssBaseline />
             {children}
-          </ThemeProvider>
-        </StylesProvider>
-      </SnackbarContextProvider>
-    </UserContextProvider>
+          </SnackbarContextProvider>
+        </UserContextProvider>
+      </StylesProvider>
+    </ThemeProvider>
   </BrowserRouter>
 );
