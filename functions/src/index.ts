bb958e9a478b52abd433220cdaee2611b799d21c
Daniel Merkl <daniel.merkl.dm@gmail.com>
2021-01-06 14:58:22 +0100

    created firebase functions to generate invitation code and delete old games

---
+++ functions/src/index.ts
@@ -0,0 +1,38 @@
+import * as firebaseFunctions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
+
+const functions = firebaseFunctions.region("europe-west3");
+
+export const generateInvitationCode = functions.https.onCall(async () => {
+  const games = await admin.firestore().collection("games").get();
+  const gameIds: Array<number> = games.docs.map((game) => Number(game.id));
+
+  const min = 1000;
+  const max = 9999;
+
+  while (true) {
+    const generatedCode = Math.floor(Math.random() * (max - min)) + min;
+    if (!gameIds.includes(generatedCode)) {
+      return generatedCode;
+    }
+  }
+});
+
+/**
+ * This database triggered function will check for games that are older than the
+ * cut-off time. Each game needs to have a `createdAt` attribute.
+ * see: https://github.com/firebase/functions-samples/tree/master/delete-old-child-nodes
+ */
+export const deleteOldGames = functions.firestore
+  .document("/games/{gameId}")
+  .onCreate(async (createdGame) => {
+    const yesterday: Date = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const gamesCollectionReference = createdGame.ref.parent;
+    const games = await gamesCollectionReference
+      .where("createdAt", "<", yesterday)
+      .get();
+    games.forEach((oldGame) => oldGame.ref.delete());
+  });
