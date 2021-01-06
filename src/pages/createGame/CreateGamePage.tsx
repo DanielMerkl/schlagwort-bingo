025275934dec9681c746d7a99b6367d13e3b36dc
Daniel Merkl <daniel.merkl.dm@gmail.com>
2021-01-06 22:01:34 +0100

    implemented create game page

--- src/pages/createGame/CreateGamePage.tsx
+++ src/pages/createGame/CreateGamePage.tsx
@@ -5,49 +5,60 @@ import styled from "styled-components";
 import { GameSizeStep } from "./GameSizeStep";
 import { AddBuzzwordsStep } from "./AddBuzzwordsStep";
 import { useSnackbar } from "../../context/SnackbarContext";
-import { ShareLinkStep } from "./ShareLinkStep";
+import { ShareInvitationCodeStep } from "./ShareInvitationCodeStep";
+import { Api } from "../../utils/Api";
+import { LoadingButton } from "../../components/LoadingButton";
 
 export const CreateGamePage: FC = () => {
-  const { showError } = useSnackbar();
+  const { showError, showSuccess } = useSnackbar();
 
   const [activeStep, setActiveStep] = useState(0);
   const [gameSize, setGameSize] = useState(9);
   const [buzzwords, setBuzzwords] = useState<Array<string>>([]);
-  const [firstStepCompleted, setFirstStepCompleted] = useState(false);
-  const [secondStepCompleted, setSecondStepCompleted] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [invitationCode, setInvitationCode] = useState<number | null>(null);
 
-  const handleNextClick = () => {
-    if (activeStep === 1 && buzzwords.length !== gameSize) {
+  const gameWasCreated: boolean = invitationCode !== null;
+
+  const handleCreateGameClick = async () => {
+    if (buzzwords.length !== gameSize) {
       const fewOrMany = buzzwords.length < gameSize ? "wenige" : "viele";
       showError(`Zu ${fewOrMany} Schlagwörter ausgewählt.`);
       return;
     }
 
-    if (activeStep === 0 && !firstStepCompleted) {
-      setFirstStepCompleted(true);
+    setIsLoading(true);
+    try {
+      const game = await Api.createGame(buzzwords);
+      setInvitationCode(game.invitationCode);
+      setActiveStep((prevState) => prevState + 1);
+      showSuccess("Spiel erfolgreich erstellt");
+    } catch (e) {
+      showError("Fehler beim Erstellen des Spiels.");
     }
-    if (activeStep === 1 && !secondStepCompleted) {
-      setSecondStepCompleted(true);
-    }
-    setActiveStep((prevState) => prevState + 1);
+    setIsLoading(false);
+  };
+
+  const handleStartGameClick = () => {
+    // TODO: implement
   };
 
   return (
     <PageWrapper>
       <Stepper alternativeLabel activeStep={activeStep}>
-        <Step completed={firstStepCompleted}>
+        <Step disabled={gameWasCreated}>
           <StepButton onClick={() => setActiveStep(0)}>
             Größe des Spiels festlegen
           </StepButton>
         </Step>
-        <Step>
+        <Step disabled={gameWasCreated}>
           <StepButton onClick={() => setActiveStep(1)}>
             Schlagwörter hinzufügen ({buzzwords.length}/{gameSize})
           </StepButton>
         </Step>
         <Step>
           <StepButton onClick={() => setActiveStep(2)}>
-            Link für Mitspieler speichern
+            Mitspieler einladen
           </StepButton>
         </Step>
       </Stepper>
@@ -61,22 +72,38 @@ export const CreateGamePage: FC = () => {
           gameSize={gameSize}
         />
       )}
-      {activeStep === 2 && <ShareLinkStep />}
+      {activeStep === 2 && (
+        <ShareInvitationCodeStep invitationCode={invitationCode} />
+      )}
       <Pagination>
         <Button
           variant="outlined"
           onClick={() => setActiveStep((prevState) => prevState - 1)}
-          disabled={activeStep === 0}
+          disabled={activeStep === 0 || activeStep === 2}
         >
           Zurück
         </Button>
-        {activeStep === 2 ? (
-          <Button variant="contained" color="primary">
-            Spiel starten
-          </Button>
-        ) : (
-          <Button variant="contained" color="primary" onClick={handleNextClick}>
+        {activeStep === 0 && (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setActiveStep((prevState) => prevState + 1)}
+          >
             Weiter
+          </Button>
+        )}
+        {activeStep === 1 && (
+          <LoadingButton isLoading={isLoading} onClick={handleCreateGameClick}>
+            Spiel erstellen
+          </LoadingButton>
+        )}
+        {activeStep === 2 && (
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={handleStartGameClick}
+          >
+            Spiel starten
           </Button>
         )}
       </Pagination>
